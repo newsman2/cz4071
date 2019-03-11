c9da89d6bb5f77a774cd70b9164e9bef2a034f00
Edwin Lim <edwinlim_11@hotmail.com>
2019-03-11 22:44:33 +0800

    group all results into 1 page

--- ns/home/forms.py
+++ ns/home/forms.py
@@ -2,10 +2,8 @@ from django import forms
 from django.forms import TextInput, PasswordInput, SelectDateWidget, RadioSelect, DateInput, Select, FileInput
 from .models import Network
 
-
+'''
 class UploadNetworkForm(forms.ModelForm):
-    # network_file = forms.Widget(widget=forms.FileInput(attrs={'class': 'form-control', 'id': 'network_file'}), label='Network File')
-
     class Meta:
         model = Network
         fields = ['network_file']
@@ -20,3 +18,19 @@ class GenerateRandomNetworkForm(forms.Form):
     probability = forms.FloatField(
         widget=forms.TextInput(
             attrs={'class': 'form-control', 'id': 'probability', 'placeholder': 'Probability'}), required=False)
+'''
+
+
+class NetworkForm(forms.ModelForm):
+    number_of_nodes = forms.IntegerField(
+        widget=forms.TextInput(
+            attrs={'class': 'form-control', 'id': 'number_of_nodes', 'placeholder': '# of Nodes'}), required=True)
+    probability = forms.FloatField(
+        widget=forms.TextInput(
+            attrs={'class': 'form-control', 'id': 'probability', 'placeholder': 'Probability'}), required=True)
+    network_file = forms.FileField(widget=forms.FileInput(
+        attrs={'class': 'form-control-file', 'id': 'network_file', 'label': 'Network File'}))
+
+    class Meta:
+        model = Network
+        fields = ['network_file']
--- ns/home/views.py
+++ ns/home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
-from .forms import UploadNetworkForm, GenerateRandomNetworkForm
+from .forms import NetworkForm
 import pandas as pd
 from ns import settings
 from django.shortcuts import redirect
@@ -13,38 +13,105 @@ from multiprocessing import Pool
 # Create your views here.
 class HomeView(View):
     template_name = 'home/index.html'
-    form_class_upload = UploadNetworkForm
-    form_class_generate = GenerateRandomNetworkForm
+    # form_class_upload = UploadNetworkForm
+    # form_class_generate = GenerateRandomNetworkForm
+    form_class_network = NetworkForm
 
     def get(self, request):
-        form_upload = self.form_class_upload(None)
-        form_generate = self.form_class_generate(None)
+        # form_upload = self.form_class_upload(None)
+        # form_generate = self.form_class_generate(None)
+        form_network = self.form_class_network(None)
 
-        return render(request, self.template_name, {'form_upload': form_upload, 'form_generate': form_generate})
+        return render(request, self.template_name, {'form_network': form_network})
 
     def post(self, request):
-        form_upload = self.form_class_upload(request.POST, request.FILES)
-        form_generate = self.form_class_generate(request.POST)
-        if 'btn_upload' in request.POST:
-            if form_upload.is_valid():
-                file = form_upload.save(commit=False)
-                network_file = request.FILES['network_file']
-                network_filename = request.FILES['network_file'].name
+        form_network = self.form_class_network(request.POST, request.FILES)
+        if form_network.is_valid():
+            file = form_network.save(commit=False)
 
-                file_name, file_format = network_filename.rsplit(".", maxsplit=1)
-                file.save()
-                convert_txt_to_csv(file_name, file_format, network_file)
-            return redirect('home:RealNetworkResults', network_filename=file_name)
+            network_file = request.FILES['network_file']
+            network_filename = request.FILES['network_file'].name
 
-        elif 'btn_generate' in request.POST:
-            if form_generate.is_valid():
-                n = request.POST['number_of_nodes']
-                p = request.POST['probability']
+            r_nodes = request.POST['number_of_nodes']
+            r_prob = request.POST['probability']
 
-            url = '/results/random/?nodes=' + n + '&prob=' + p
-            return redirect(url)
+            file_name, file_format = network_filename.rsplit(".", maxsplit=1)
+            file.save()
+            convert_txt_to_csv(file_name, file_format, network_file)
 
-        return render(request, self.template_name, {'form_upload': form_upload, 'form_generate': form_generate})
+            return redirect('home:NetworkResultsView', r_nodes=r_nodes, r_prob=r_prob, network_filename=file_name)
+
+        # form_upload = self.form_class_upload(request.POST, request.FILES)
+        # form_generate = self.form_class_generate(request.POST)
+        # if 'btn_upload' in request.POST:
+        #     if form_upload.is_valid():
+        #         file = form_upload.save(commit=False)
+        #         network_file = request.FILES['network_file']
+        #         network_filename = request.FILES['network_file'].name
+        #
+        #         file_name, file_format = network_filename.rsplit(".", maxsplit=1)
+        #         file.save()
+        #         convert_txt_to_csv(file_name, file_format, network_file)
+        #     return redirect('home:RealNetworkResults', network_filename=file_name)
+        #
+        # elif 'btn_generate' in request.POST:
+        #     if form_generate.is_valid():
+        #         n = request.POST['number_of_nodes']
+        #         p = request.POST['probability']
+        #
+        #     url = '/results/random/?nodes=' + n + '&prob=' + p
+        #     return redirect(url)
+
+        return render(request, self.template_name, {'form_network': form_network})
+
+
+class NetworkResultsView(View):
+    template_name = 'home/network_results.html'
+
+    def get(self, request, r_nodes, r_prob, network_filename):
+        #n = int(request.GET.get('nodes', '5'))
+        #p = float(request.GET.get('prob', '0.5'))
+        r_nodes = int(r_nodes)
+        r_prob = float(r_prob)
+        random_properties = _compute_random_network_properties('Random_Network', r_nodes, r_prob)
+
+        network = _load_graph_csv_from_file_system(network_filename)
+        is_too_big = _is_network_too_big(network.number_of_nodes(), network.number_of_edges(), network,
+                                         network_filename)
+        scale_properties = _compute_scale_free_properties(network)
+        real_properties = _compute_real_network_properties(network_filename, network)
+
+        return render(request, self.template_name, {
+            'ra_p': random_properties['p'],
+            'ra_expected_no_of_nodes': random_properties['expected_no_of_nodes'],
+            'ra_expected_no_of_edges': random_properties['expected_no_of_edges'],
+            'ra_expected_degree_distribution_plot_file_name': random_properties['expected_degree_distribution_plot_file_name'],
+            'ra_degree_distribution_plot_path': settings.MEDIA_URL + '/plot/' + random_properties[
+                'expected_degree_distribution_plot_file_name'],
+            'ra_expected_average_degree': random_properties['expected_average_degree'],
+            'ra_expected_regime_type': random_properties['expected_regime_type'],
+            'ra_expected_average_distance': random_properties['expected_average_distance'],
+            'ra_expected_clustering_coefficient': random_properties['expected_clustering_coefficient'],
+            'ra_plot': random_properties['interactive_network_plot'],
+
+            're_is_too_big': is_too_big,
+            're_no_of_nodes': real_properties['no_of_nodes'],
+            're_no_of_edges': real_properties['no_of_edges'],
+            're_degree_distribution_plot_path_file_name': network_filename,
+            're_degree_distribution_plot_path': settings.MEDIA_URL + 'plot/' + network_filename + '_degree_distribution_log_binning.png',
+
+            're_average_degree': real_properties['average_degree'],
+            're_degree_second_moment': real_properties['degree_second_moment'],
+            're_real_kmax': real_properties['real_kmax'],
+            're_real_kmin': real_properties['real_kmin'],
+            're_global_clustering_coefficient': real_properties['global_clustering_coefficient'],
+            're_average_clustering_coefficient': real_properties['average_clustering_coefficient'],
+
+            'sc_degree_exponent': scale_properties['degree_exponent'],
+            'sc_expected_kmax': scale_properties['expected_kmax'],
+            'sc_expected_average_distance': scale_properties['expected_average_distance'],
+            'sc_expected_degree_exponent': scale_properties['expected_degree_exponent']
+        })
 
 
 class RandomNetworkResultsView(View):
